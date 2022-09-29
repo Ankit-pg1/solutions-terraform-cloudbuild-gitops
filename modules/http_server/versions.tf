ab52572fa6393b2c89b9026bf72332e3cf5feb33
Ankit Patel <105269721+Ankit-pg1@users.noreply.github.com>
2022-09-29 17:56:44 +0530

    Update versions.tf

--- modules/http_server/versions.tf
+++ modules/http_server/versions.tf
@@ -14,5 +14,14 @@
 
 
 terraform {
-  required_version = "~> 1.0.0"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+    google-beta = {
+      source = "hashicorp/google-beta"
+    }
+  }
+  required_version = ">= 0.13"
 }
+
